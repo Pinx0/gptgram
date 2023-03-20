8670597dce70686b5549eafc29cc5de8b9f830e9
Pinx0 <plponce@gmail.com>
2023-03-20 23:56:27 +0100

    Move openAI validator to its service

--- bot/services/openai.ts
+++ bot/services/openai.ts
@@ -1,106 +1,117 @@
-import type { Chat, User, Message } from '../../generated/client'
-import type { Bot } from "../config/bots";
+import type { Chat, User, Message } from '../../generated/client';
+import type { Bot } from '../config/bots';
 
-type GptMessage = { role: "system" | "user" | "assistant"; content: string };
+type GptMessage = { role: 'system' | 'user' | 'assistant'; content: string };
 
 export type Prompt = {
-  messages: GptMessage[];
+    messages: GptMessage[];
 };
 
 type GetGptResponseCommand = {
-  prompt: Prompt;
-  chatConfig: Chat;
+    prompt: Prompt;
+    chatConfig: Chat;
 };
 
 type GptChoice = {
-  message: {
-    role: "assistant";
-    content: string;
-  };
-  finish_reason: string;
-  index: number;
+    message: {
+        role: 'assistant';
+        content: string;
+    };
+    finish_reason: string;
+    index: number;
 };
 
 export type GptValidResponse = {
-  usage: {
-    prompt_tokens: number;
-    completion_tokens: number;
-  };
-  created: number;
-  id: string;
-  model: string;
-  choices: GptChoice[];
+    usage: {
+        prompt_tokens: number;
+        completion_tokens: number;
+    };
+    created: number;
+    id: string;
+    model: string;
+    choices: GptChoice[];
 };
 type GptInvalidResponse = {
-  error: {
-    message: string;
-    code: string;
-  };
+    error: {
+        message: string;
+        code: string;
+    };
 };
 type GptResult = GptValidResponse | GptInvalidResponse;
-export const getGptResponse = async (
-  requestBody: GetGptResponseCommand
-): Promise<GptResult> => {
-  const { prompt, chatConfig } = requestBody;
-  try {
-    const body = JSON.stringify({
-      messages: prompt.messages,
-      max_tokens: chatConfig.token_limit,
-      temperature: chatConfig.temperature,
-      frequency_penalty: chatConfig.frequency_penalty,
-      presence_penalty: chatConfig.presence_penalty,
-      model: chatConfig.model,
-    });
-    console.log("OpenAI Request", body)
-    const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: chatConfig.api_key ?? "",
-      },
-      body,
-    });
-    return (await response.json()) as GptValidResponse | GptInvalidResponse;
-  } catch (error) {
-    return { error: { message: error as string, code: "unknown" } };
-  }
+export const getGptResponse = async (requestBody: GetGptResponseCommand): Promise<GptResult> => {
+    const { prompt, chatConfig } = requestBody;
+    try {
+        const body = JSON.stringify({
+            messages: prompt.messages,
+            max_tokens: chatConfig.token_limit,
+            temperature: chatConfig.temperature,
+            frequency_penalty: chatConfig.frequency_penalty,
+            presence_penalty: chatConfig.presence_penalty,
+            model: chatConfig.model,
+        });
+        console.log('OpenAI Request', body);
+        const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: chatConfig.api_key ?? '',
+            },
+            body,
+        });
+        return (await response.json()) as GptValidResponse | GptInvalidResponse;
+    } catch (error) {
+        return { error: { message: error as string, code: 'unknown' } };
+    }
 };
 type MessageWithUser = Message & {
-  user: User;
+    user: User;
 };
 
-export const preparePrompt = (
-  chatMessages: MessageWithUser[],
-  chatConfig: Chat,
-  bot: Bot
-): Prompt => {
-  const messages: GptMessage[] = [];
-  //add system message
-  messages.push({
-    role: "system",
-    content: chatConfig.personality ?? "You are a chatbot.",
-  });
-  //add bot first message to set tone
-  if (chatConfig.first_message) {
+export const preparePrompt = (chatMessages: MessageWithUser[], chatConfig: Chat, bot: Bot): Prompt => {
+    const messages: GptMessage[] = [];
+    //add system message
     messages.push({
-      role: "assistant",
-      content: chatConfig.bot_name + ": " + chatConfig.first_message,
+        role: 'system',
+        content: chatConfig.personality ?? 'You are a chatbot.',
     });
-  }
-  //add all messages in chronological order
-  chatMessages
-    .sort((a, b) => a.date - b.date)
-    .map((m) => {
-      const isBotMessage = m.user_id === BigInt(bot.id);
-      const role = isBotMessage ? "assistant" : "user";
-      const name = isBotMessage ? chatConfig.bot_name : m.user.first_name;
-      const processedMessage =
-        name + ": " + m.content.replace(bot.username, chatConfig.bot_name);
+    //add bot first message to set tone
+    if (chatConfig.first_message) {
+        messages.push({
+            role: 'assistant',
+            content: chatConfig.bot_name + ': ' + chatConfig.first_message,
+        });
+    }
+    //add all messages in chronological order
+    chatMessages
+        .sort((a, b) => a.date - b.date)
+        .map((m) => {
+            const isBotMessage = m.user_id === BigInt(bot.id);
+            const role = isBotMessage ? 'assistant' : 'user';
+            const name = isBotMessage ? chatConfig.bot_name : m.user.first_name;
+            const processedMessage = name + ': ' + m.content.replace(bot.username, chatConfig.bot_name);
 
-      messages.push({
-        role: role,
-        content: processedMessage,
-      });
-    });
-  return { messages };
+            messages.push({
+                role: role,
+                content: processedMessage,
+            });
+        });
+    return { messages };
+};
+
+type ValidationOkResult = {
+    validConfig: Chat;
+};
+type ValidationErrorResult = {
+    errors: string[];
+};
+type ValidationResult = ValidationOkResult | ValidationErrorResult;
+export const validateConfig = (chatConfig: Chat): ValidationResult => {
+    const errors: string[] = [];
+    if (!chatConfig.api_key) errors.push('Missing API Key');
+    if (!chatConfig.personality) errors.push('Missing Personality');
+    if (chatConfig.token_limit > 1000) errors.push('Token limit exceeded maximum (1000)');
+    if (chatConfig.token_limit < 50) errors.push('Token limit below minimum (50)');
+    if (chatConfig.model !== 'gpt-3.5-turbo') errors.push('Invalid model');
+    if (errors.length > 0) return { errors };
+    return { validConfig: chatConfig };
 };
--- bot/services/processUpdate.ts
+++ bot/services/processUpdate.ts
@@ -1,184 +1,149 @@
-import type { Message as TelegramMessage, Update } from "@grammyjs/types";
-import { FromTelegramToCreateMessageCommand } from "./converter";
-import {
-  createMessage,
-  getChatConfig,
-  getLastClear,
-  getLastMessages,
-  saveCompletion,
-} from "./database";
-import { sendMessage } from "./telegram";
-import type { Chat } from "@prisma/client";
-import type { Bot } from "../config/bots";
-import { getGptResponse, preparePrompt } from "./openai";
+import type { Message as TelegramMessage, Update } from '@grammyjs/types';
+import { FromTelegramToCreateMessageCommand } from './converter';
+import { createMessage, getChatConfig, getLastClear, getLastMessages, saveCompletion } from './database';
+import { sendMessage } from './telegram';
+import type { Chat } from '@prisma/client';
+import type { Bot } from '../config/bots';
+import { getGptResponse, preparePrompt, validateConfig } from './openai';
 
 type Success = {
-  ok: { messageSent: boolean };
+    ok: { messageSent: boolean };
 };
 type Failure = {
-  error: "GPT" | "Invalid update" | "DB error";
+    error: 'GPT' | 'Invalid update' | 'DB error';
 };
 export type Result = Success | Failure;
 
-export const processUpdate = async (
-  bot: Bot,
-  update: Update
-): Promise<Result> => {
-  const token = bot.id.toString() + ":" + bot.secret;
-  const message = update?.message;
-  if (!message) {
-    console.log("Invalid update", update);
-    return {
-      error: "Invalid update",
-    };
-  }
-  const replyToMessage = message.reply_to_message;
-  const chat = message.chat;
-  const chat_id = chat.id;
+export const processUpdate = async (bot: Bot, update: Update): Promise<Result> => {
+    const token = bot.id.toString() + ':' + bot.secret;
+    const message = update?.message;
+    if (!message) {
+        console.log('Invalid update', update);
+        return {
+            error: 'Invalid update',
+        };
+    }
+    const replyToMessage = message.reply_to_message;
+    const chat = message.chat;
+    const chat_id = chat.id;
 
-  //convert telegram format to DB format
-  const createMessageCommand = FromTelegramToCreateMessageCommand(message, bot);
-  console.log("Message parsed");
+    //convert telegram format to DB format
+    const createMessageCommand = FromTelegramToCreateMessageCommand(message, bot);
+    console.log('Message parsed');
 
-  //log message to db
-  let dbMessage;
-  try {
-    dbMessage = await createMessage(createMessageCommand);
-    console.log("Message saved to DB");
-  } catch (error) {
-    console.log("Couldn't save message to DB", error);
-    return { error: "DB error" };
-  }
+    //log message to db
+    let dbMessage;
+    try {
+        dbMessage = await createMessage(createMessageCommand);
+        console.log('Message saved to DB');
+    } catch (error) {
+        console.log("Couldn't save message to DB", error);
+        return { error: 'DB error' };
+    }
 
-  //get chat config
-  let chatConfig = await getChatConfig({ chat_id, bot_id: bot.id });
-  console.log("Chat config", chatConfig);
+    //get chat config
+    let chatConfig = await getChatConfig({ chat_id, bot_id: bot.id });
+    console.log('Chat config', chatConfig);
 
-  if (!chatConfig) {
-    return { error: "DB error" };
-  }
+    if (!chatConfig) {
+        return { error: 'DB error' };
+    }
 
-  //check if should respond
-  const speak = shouldSpeak(message, chatConfig, bot);
-  if (!speak) return { ok: { messageSent: false } };
-  //check errors in config
-  const validationResult = validateConfig(chatConfig);
-  if ("errors" in validationResult) {
-    const responseToUser =
-      "Your config is not valid due to: " +
-      validationResult.errors.join(", ") +
-      ". You can ask @PabloLopezPonce for help.";
-    console.log("Invalid config", validationResult.errors);
-    const telegramResponse = await sendMessage({
-      token,
-      chat_id,
-      text: responseToUser,
-      reply_to_message_id: replyToMessage?.message_id,
+    //check if should respond
+    const speak = shouldSpeak(message, chatConfig, bot);
+    if (!speak) return { ok: { messageSent: false } };
+    //check errors in config
+    const validationResult = validateConfig(chatConfig);
+    if ('errors' in validationResult) {
+        const responseToUser =
+            'Your config is not valid due to: ' +
+            validationResult.errors.join(', ') +
+            '. You can ask @PabloLopezPonce for help.';
+        console.log('Invalid config', validationResult.errors);
+        const telegramResponse = await sendMessage({
+            token,
+            chat_id,
+            text: responseToUser,
+            reply_to_message_id: replyToMessage?.message_id,
+        });
+        console.log('Telegram Response', telegramResponse);
+
+        return { ok: { messageSent: true } };
+    }
+    const validConfig = validationResult.validConfig;
+
+    //get last N messages from DB
+    const lastClear = await getLastClear({ chat_id, bot_id: bot.id });
+    console.log('Last clear', lastClear);
+    const lastMessages = await getLastMessages({
+        chat_id,
+        bot_id: bot.id,
+        number_of_messages: validConfig.history_length,
+        min_date: lastClear?.date,
     });
-    console.log("Telegram Response", telegramResponse);
+    //prepare openAI prompt
+    const prompt = preparePrompt(lastMessages, validConfig, bot);
+    console.log('Prompt ready');
+    //call openAI
+    const gptResponse = await getGptResponse({ prompt, chatConfig: validConfig });
+    console.log('GPT Response', gptResponse);
 
-    return { ok: { messageSent: true } };
-  }
-  const validConfig = validationResult.validConfig;
+    if ('error' in gptResponse) {
+        if (gptResponse.error.code === 'invalid_api_key') {
+            const responseToUser = 'The API key you provided is being rejected ';
+            const telegramResponse = await sendMessage({
+                token,
+                chat_id,
+                text: responseToUser,
+                reply_to_message_id: replyToMessage?.message_id,
+            });
+            console.log('Telegram Response', telegramResponse);
+            return { ok: { messageSent: true } };
+        }
+        return { error: 'GPT' };
+    }
+    //log completion
+    await saveCompletion(gptResponse, dbMessage, bot);
 
-  //get last N messages from DB
-  const lastClear = await getLastClear({ chat_id, bot_id: bot.id });
-  console.log("Last clear", lastClear);
-  const lastMessages = await getLastMessages({
-    chat_id,
-    bot_id: bot.id,
-    number_of_messages: validConfig.history_length,
-    min_date: lastClear?.date,
-  });
-  //prepare openAI prompt
-  const prompt = preparePrompt(lastMessages, validConfig, bot);
-  console.log("Prompt ready");
-  //call openAI
-  const gptResponse = await getGptResponse({ prompt, chatConfig: validConfig });
-  console.log("GPT Response", gptResponse);
-
-  if ("error" in gptResponse) {
-    if (gptResponse.error.code === "invalid_api_key") {
-      const responseToUser = "The API key you provided is being rejected ";
-      const telegramResponse = await sendMessage({
+    const gptMessage = gptResponse.choices[0].message.content;
+    const responseToUser = trimUserNameFromMessage(gptMessage);
+    //send message
+    const telegramResponse = await sendMessage({
         token,
         chat_id,
         text: responseToUser,
         reply_to_message_id: replyToMessage?.message_id,
-      });
-      console.log("Telegram Response", telegramResponse);
-      return { ok: { messageSent: true } };
-    }
-    return { error: "GPT" };
-  }
-  //log completion
-  await saveCompletion(gptResponse, dbMessage, bot);
+    });
+    console.log('Telegram Response', telegramResponse);
+    const botMessage = telegramResponse.response?.result as TelegramMessage.TextMessage;
+    const createBotMessageCommand = FromTelegramToCreateMessageCommand(botMessage, bot);
 
-  const gptMessage = gptResponse.choices[0].message.content;
-  const responseToUser = trimUserNameFromMessage(gptMessage);
-  //send message
-  const telegramResponse = await sendMessage({
-    token,
-    chat_id,
-    text: responseToUser,
-    reply_to_message_id: replyToMessage?.message_id,
-  });
-  console.log("Telegram Response", telegramResponse);
-  const botMessage = telegramResponse.response
-    ?.result as TelegramMessage.TextMessage;
-  const createBotMessageCommand = FromTelegramToCreateMessageCommand(
-    botMessage,
-    bot
-  );
-
-  //save it to database
-  await createMessage(createBotMessageCommand);
-  return { ok: { messageSent: true } };
+    //save it to database
+    await createMessage(createBotMessageCommand);
+    return { ok: { messageSent: true } };
 };
 
-const trimUserNameFromMessage = (input:string) => {
-  const regex = /^((?:\S+\s){0,3}\S+)\s?:\s?([\s\S]*)$/;
-  const result = input.match(regex);
+const trimUserNameFromMessage = (input: string) => {
+    const regex = /^((?:\S+\s){0,3}\S+)\s?:\s?([\s\S]*)$/;
+    const result = input.match(regex);
 
-  return result && result.length > 1 && result[2] ? result[2] : input;
+    return result && result.length > 1 && result[2] ? result[2] : input;
 };
 
 const shouldSpeak = (message: TelegramMessage, chatConfig: Chat, bot: Bot) => {
-  if (message.chat.type === "private") {
-    console.log("Is private chat");
-    return true;
-  }
-  if (
-    message.reply_to_message?.from?.id &&
-    BigInt(message.reply_to_message?.from?.id) === chatConfig.bot_id
-  ) {
-    console.log("Is replying to bot message");
-    return true;
-  }
-  if (message.text && message.text.includes(bot.username)) {
-    console.log("Is mentioning the bot");
-    return true;
-  }
-  const dice = Math.random();
-  console.log(`Dice: ${dice} - Speak chance: ${chatConfig.speak_chance}`);
-  return dice < chatConfig.speak_chance;
-};
-type ValidationOkResult = {
-  validConfig: Chat;
-};
-type ValidationErrorResult = {
-  errors: string[];
-};
-type ValidationResult = ValidationOkResult | ValidationErrorResult;
-const validateConfig = (chatConfig: Chat): ValidationResult => {
-  const errors: string[] = [];
-  if (!chatConfig.api_key) errors.push("Missing API Key");
-  if (!chatConfig.personality) errors.push("Missing Personality");
-  if (chatConfig.token_limit > 1000)
-    errors.push("Token limit exceeded maximum (1000)");
-  if (chatConfig.token_limit < 50)
-    errors.push("Token limit below minimum (50)");
-  if (chatConfig.model !== "gpt-3.5-turbo") errors.push("Invalid model");
-  if (errors.length > 0) return { errors };
-  return { validConfig: chatConfig };
+    if (message.chat.type === 'private') {
+        console.log('Is private chat');
+        return true;
+    }
+    if (message.reply_to_message?.from?.id && BigInt(message.reply_to_message?.from?.id) === chatConfig.bot_id) {
+        console.log('Is replying to bot message');
+        return true;
+    }
+    if (message.text && message.text.includes(bot.username)) {
+        console.log('Is mentioning the bot');
+        return true;
+    }
+    const dice = Math.random();
+    console.log(`Dice: ${dice} - Speak chance: ${chatConfig.speak_chance}`);
+    return dice < chatConfig.speak_chance;
 };
